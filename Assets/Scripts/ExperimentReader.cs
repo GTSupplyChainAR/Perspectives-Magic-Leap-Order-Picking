a3f593cd6f254b03dad1873c60535e846fdd5341
tomthecarrot <code@tomthecarrot.com>
2019-04-13 00:17:07 -0400

    Fix ExperimentReader initial read/parse

--- Assets/Scripts/ExperimentReader.cs
+++ Assets/Scripts/ExperimentReader.cs
@@ -7,8 +7,6 @@ public class ExperimentReader {
     private Participant[] participantsArr;
     private int participantId;
     private PathIds pathIds;
-    private PickPaths paths;
-    private PickPath[] patharr;
 
     public ExperimentReader(string filePath)
     {
@@ -16,14 +14,14 @@ public class ExperimentReader {
         if (File.Exists(filePath))
         {
             string dataAsJSON = File.ReadAllText(filePath);
-            paths = JsonUtility.FromJson<PickPaths>(dataAsJSON);
-            Debug.Log(paths);
-            patharr = paths.pickPaths;
-            Debug.Log(patharr);
+            participants = JsonUtility.FromJson<Participants>(dataAsJSON);
+            Debug.Log(participants);
+            participantsArr = participants.participants;
+            Debug.Log(participantsArr);
 
-            Debug.Log(patharr.Length);
-            foreach (PickPath path in patharr) {
-                Debug.Log(path);
+            Debug.Log(participantsArr.Length);
+            foreach (Participant participant in participantsArr) {
+                Debug.Log(participant.testingPathOrder[0].pathIds[0].pathId);
             }
         } else
         {
